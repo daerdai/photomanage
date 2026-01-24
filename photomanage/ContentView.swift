a04f3abe1a1c0ab05009bf024821a0f3156c0e97
daerdai <dingxiangzhong1015@gmail.com>
2026-01-24 08:33:31 +0800

    Initial Commit

---
+++ photomanage/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  photomanage
+//
+//  Created by daerdai on 2026/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ photomanage/photomanageApp.swift
@@ -0,0 +1,17 @@
+//
+//  photomanageApp.swift
+//  photomanage
+//
+//  Created by daerdai on 2026/1/24.
+//
+
+import SwiftUI
+
+@main
+struct photomanageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
